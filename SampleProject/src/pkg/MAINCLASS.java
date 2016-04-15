5c0a0f85221f3ff4c2e8a3c4a11e4aa867dfad97
Ankur Agarwal <ankur.agarwal@snapdeal.com>
2016-04-15 12:44:12 +0530

    For average frame rates

--- SampleProject/src/pkg/MAINCLASS.java
+++ SampleProject/src/pkg/MAINCLASS.java
@@ -18,7 +18,7 @@ public class MAINCLASS {
 	
 	static String temp1 = "[\r\n  {\r\n    \"name\": \"table_name\",\r\n    \"columns\": [\"draw\", \"prepare\", \"process\", \"execute\"],\r\n    \"points\": [";
 	static String temp2 = "]\r\n  }\r\n]";
-	String DRAW, PREPARE, PROCESS, EXECUTE;
+	String DRAW="0", PREPARE="0", PROCESS="0", EXECUTE="0";
 	
 	
 	public static void main(String[] args) throws JSONException, JsonProcessingException, IOException, InterruptedException, URISyntaxException {
@@ -28,11 +28,18 @@ public class MAINCLASS {
 		for(int i=0; i<10; i++) {
 			Thread.sleep(2000);
 			
-			String measurements = m.prepareMeasurements(m.readArray());
+			ArrayList<String[]> array = m.readArray();
+			String measurements = "";
+			String avgMeasurement = "";
+			if(array.size() > 0){
+				measurements = m.prepareMeasurements(array);
+				avgMeasurement = m.prepareAverageMeasurement(array);
+			}
 			
 			System.out.println(measurements);
 			System.out.println();
-			m.postRequest(measurements);
+			m.postRequest("http://10.20.86.83:8086/write?db=myDB&u=admin&p=admin", measurements);
+			m.postRequest("http://10.20.86.83:8086/write?db=myDB&u=admin&p=admin", avgMeasurement);
 		}
 	}
 	
@@ -43,18 +50,37 @@ public class MAINCLASS {
 		long del = 2000/(size+1);
 		for(int i=0; i<size; i++) {
 			String[] a = data.get(i);
-			DRAW = a[1];
-			PREPARE = a[2];
-			PROCESS = a[3];
-			EXECUTE = a[4];
+			if(a.length == 5){
+				DRAW = a[1];
+				PREPARE = a[2];
+				PROCESS = a[3];
+				EXECUTE = a[4];
+			}
 			time += del;
 			s = s + "frames " + "draw="+DRAW+ ",prepare="+PREPARE+ ",process="+PROCESS+ ",execute="+EXECUTE +" "+time+"000000"+"\n";
 		}
 		return s;
 	}
 	
-	public void postRequest (String str) throws IOException, URISyntaxException {
-		URI url = new URI("http://10.20.86.83:8086/write?db=myDB&u=admin&p=admin");
+	public String prepareAverageMeasurement(ArrayList<String[]> data) {
+		int size = data.size();
+		String s = "";
+		float f_draw=0, f_prepare=0, f_process=0, f_execute=0;
+		for(int i=0; i<size; i++) {
+			String[] a = data.get(i);
+			if(a.length == 5) {
+				f_draw = f_draw + Float.valueOf(a[1]);
+				f_prepare = f_prepare + Float.valueOf(a[2]);
+				f_process = f_process + Float.valueOf(a[3]);
+				f_execute = f_execute + Float.valueOf(a[4]);
+			}
+		}
+		s = "Avg_Frames draw="+(f_draw/(size))+",prepare="+(f_prepare/(size+1))+ ",process="+(f_process/(size+1))+ ",execute="+(f_execute/(size+1));
+		return s;
+	}
+	
+	public void postRequest (String url1, String str) throws IOException, URISyntaxException {
+		URI url = new URI(url1);
 		HttpPost post = new HttpPost(url);
 		HttpClient client = HttpClientBuilder.create().build();
 		post.addHeader("Content-Type", "text");
